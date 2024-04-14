ce10ac32072d876bc7e06c42c8782acd650ffb7a
OrlayMolina <111409267+OrlayMolina@users.noreply.github.com>
2024-04-14 12:57:10 -0500

    Se comunica el boton de login con la pagina de registro.

--- src/main/java/co/edu/uniquindio/estructura/datos/sala/urgencias/MainUrgencias.java
+++ src/main/java/co/edu/uniquindio/estructura/datos/sala/urgencias/MainUrgencias.java
@@ -2,6 +2,7 @@ package co.edu.uniquindio.estructura.datos.sala.urgencias;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.image.Image;
 import javafx.stage.Stage;
@@ -18,8 +19,28 @@ public class MainUrgencias extends Application {
         Image iconImage = new Image(Objects.requireNonNull(getClass().getResource(rutaRelativa)).toExternalForm());
         stage.getIcons().add(iconImage);
         stage.setTitle("Ingresa con tus datos de usuario");
+        stage.setResizable(false);
         stage.setScene(scene);
         stage.show();
+    }
+    public void cargarVentanaRegistroPacientes() {
+        try {
+            FXMLLoader fxmlLoader = new FXMLLoader(MainUrgencias.class.getResource("registro-pacientes-view.fxml"));
+            Parent root = fxmlLoader.load();
+            Scene scene = new Scene(root);
+            Stage newStage = new Stage();
+            newStage.centerOnScreen();
+            String rutaRelativa = "/co/edu/uniquindio/estructura/datos/sala/urgencias/img/logo.JPG";
+            Image iconImage = new Image(Objects.requireNonNull(getClass().getResource(rutaRelativa)).toExternalForm());
+            newStage.getIcons().add(iconImage);
+            newStage.setTitle("Registro de Pacientes a Sala de Urgencias");
+            newStage.setResizable(false);
+            newStage.setScene(scene);
+            newStage.show();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
     public static void main(String[] args) {
--- src/main/java/co/edu/uniquindio/estructura/datos/sala/urgencias/viewController/LoginViewController.java
+++ src/main/java/co/edu/uniquindio/estructura/datos/sala/urgencias/viewController/LoginViewController.java
@@ -1,11 +1,18 @@
 package co.edu.uniquindio.estructura.datos.sala.urgencias.viewController;
 
+import co.edu.uniquindio.estructura.datos.sala.urgencias.MainUrgencias;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
 
 public class LoginViewController {
+
+    MainUrgencias urgencias = new MainUrgencias();
+
 
     @FXML
     private Button btnCancelarLogin;
@@ -18,5 +25,30 @@ public class LoginViewController {
 
     @FXML
     private TextField txfNombreUsuario;
+
+    @FXML
+    void siguienteVentana(ActionEvent event) {
+        inicioSesion();
+    }
+
+    private void inicioSesion(){
+        String usuario = txfNombreUsuario.getText();
+        String password = txfContrasenia.getText();
+
+        cerrarVentana(btnIngresar);
+        urgencias.cargarVentanaRegistroPacientes();
+    }
+
+    private void mostrarMensaje(String titulo, String header, String contenido, Alert.AlertType alertType) {
+        Alert aler = new Alert(alertType);
+        aler.setTitle(titulo);
+        aler.setHeaderText(header);
+        aler.setContentText(contenido);
+        aler.showAndWait();
+    }
+    public void cerrarVentana(Button btn) {
+        Stage stage = (Stage) btn.getScene().getWindow();
+        stage.close();
+    }
 
 }
