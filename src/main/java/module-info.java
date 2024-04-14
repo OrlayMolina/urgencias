c6fff5f3bd6b029556ec9c3ee7cafbf272a9a42b
OrlayMolina <111409267+OrlayMolina@users.noreply.github.com>
2024-04-13 23:04:04 -0500

    Se inicia la pantalla de registro de pacientes.

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -11,6 +11,8 @@ module co.edu.uniquindio.estructura.datos.sala.urgencias {
     exports co.edu.uniquindio.estructura.datos.sala.urgencias.models;
     opens co.edu.uniquindio.estructura.datos.sala.urgencias.enumms to javafx.fxml;
     exports co.edu.uniquindio.estructura.datos.sala.urgencias.enumms;
-    //opens co.edu.uniquindio.estructura.datos.sala.urgencias.utils to javafx.fxml;
-    //exports co.edu.uniquindio.estructura.datos.sala.urgencias.utils;
+    opens co.edu.uniquindio.estructura.datos.sala.urgencias.utils to javafx.fxml;
+    exports co.edu.uniquindio.estructura.datos.sala.urgencias.utils;
+    opens co.edu.uniquindio.estructura.datos.sala.urgencias.controller to javafx.fxml;
+    exports co.edu.uniquindio.estructura.datos.sala.urgencias.controller;
 }
